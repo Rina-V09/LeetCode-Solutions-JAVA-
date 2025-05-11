96868197a912cb8cf8e2761555e00529ac7c47cf
Rina <rinavadera2003@gmail.com>
2025-05-11 22:06:15 +0530

    Create 1550.ThreeConsecutiveOdds.java

---
+++ 1550.ThreeConsecutiveOdds.java
@@ -0,0 +1,18 @@
+class Solution{
+  public boolean threeConsecutiveOdds(int[] arr) {
+    int n = arr.length;
+    int oddCount = 0;
+
+    for(int i=0; i<n; i++){
+      if(i%2!=0){
+        oddCount++;
+        if( oddCount == 3){
+          return true;
+        }
+      }else{
+        oddCount = 0;
+      }
+    }
+    return false;
+  }
+}
