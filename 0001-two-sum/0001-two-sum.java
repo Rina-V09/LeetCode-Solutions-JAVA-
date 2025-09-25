f8e1458f9affac78973c8d223ac1401b1d62f33a
Rina <rinavadera2003@gmail.com>
2025-09-25 10:19:59 +0530

    Time: 8 ms (50.64%), Space: 45.2 MB (31.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        int[][] arr = new int[n][2];
+        int[][] mat = new int[n][2];
 
         for(int i=0; i<n; i++){
-            arr[i][0]=nums[i];
-            arr[i][1]=i; //org idx
-        }
+            mat[i][0] = nums[i];
+            mat[i][1] = i;
+        } 
 
-        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
-        int l=0, r=n-1;
-
+        Arrays.sort(mat,(a,b) -> Integer.compare(a[0], b[0]));
+        int l = 0, r = n-1;
         while(l<r){
-            if(arr[l][0] + arr[r][0] == target){
-                return new int[] {arr[l][1],arr[r][1]};
-            }else if(arr[l][0] + arr[r][0] < target){
+            if(mat[l][0] + mat[r][0] == target){
+                return new int[]{mat[l][1], mat[r][1]};
+            }else if(mat[l][0] + mat[r][0] < target){
                 l++;
             }else{
                 r--;
             }
         }
-        return new int[] {};
+
+        return new int[]{};
     }
 }
